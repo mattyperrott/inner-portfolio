b5d83cf62fdf37d155d66b8eb4aaa48c5d6d28b6
mattyperrott <104394280+mattyperrott@users.noreply.github.com>
2023-06-03 04:00:21 +1000

    Update About.tsx

--- src/components/showcase/About.tsx
+++ src/components/showcase/About.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import me from '../../assets/pictures/workingAtComputer.jpg';
 import meNow from '../../assets/pictures/currentme.jpg';
 import { Link } from 'react-router-dom';
 
@@ -9,7 +8,7 @@ const About: React.FC<AboutProps> = (props) => {
     return (
         // add on resize listener
         <div className="site-page-content">
-            {/* <img src={me} style={styles.topImage} alt="" /> */}
+
             <h1 style={{ marginLeft: -16 }}>Welcome</h1>
             <h3>I'm Matty P</h3>
             <br />
@@ -18,54 +17,9 @@ const About: React.FC<AboutProps> = (props) => {
                 As a digital designer and developer, my work is an intersection of creativity and technology, driven by a deep understanding of user-centric design, responsiveness, and performance.
                 </p>
                 <br />
-                <p>
-                    Thank you for taking the time to check out my portfolio. I
-                    really hope you enjoy exploring it as much as I enjoyed
-                    building it. If you have any questions or comments, feel
-                    free to contact me using{' '}
-                    <Link to="/contact">this form</Link> or shoot me an email at{' '}
-                    <a href="mailto:email@mattyp.com.au">
-                        email@mattyp.com.au
-                    </a>
-                </p>
             </div>
 
-            <div className="text-block">
-                <h3>About Me</h3>
-                <br />
-                <p>
-                  My journey into the tech industry is anything but conventional.
-                  Without a traditional educational pathway, my career has been driven by a relentless curiosity, a passion for technology and design, and a drive for constant self-learning.
-                </p>
-                <br />
-                <p>
-With a firm grasp on popular designing tools and coding languages, and a continuous drive for learning new technologies,
-I have built a comprehensive portfolio that reflects a blend of eye-catching aesthetics and functionality.
-Over the years, I have honed my skills by immersing myself in an array of freelance projects and collaborations, resulting in a versatile skill set that spans across web design and development, branding, graphic design, marketing, and software engineering - all without a traditional degree.
-
-                </p>
-                <div className="captioned-image">
-                    <img src={me} style={styles.image} alt="" />
-                    <p>
-                        <sub>
-                            <b>Figure 1:</b> me.
-                        </sub>
-                    </p>
-                </div>
-
-                <p>
-                  I am a blend of a creative and logical thinker. My creativity sparks innovative ideas, while my logical side ensures their feasibility and alignment with any business objective.
-                 I believe this balance is essential in creating digital solutions that are not only visually stunning but also technically robust.
-                </p>
-                <br />
-                <p>
-                  As a Melbournian, I draw inspiration from my vibrant surroundings and multicultural city, translating my experiences into digital mediums.
-                  The desire to create meaningful and engaging digital interactions has been the north star guiding my career progression.
-                  My work is characterised by meticulous attention to detail, commitment to design ethics, and an ability to translate client requirements into visually compelling and functional designs.
-                </p>
-                <br />
-                <br />
-                <div style={{}}>
+                 <div style={{}}>
                     <div
                         style={{
                             flex: 1,
@@ -75,13 +29,23 @@ Over the years, I have honed my skills by immersing myself in an array of freela
                         }}
                     >
 
-                  <strong>  <p>
-                      I've created diverse websites, meticulously balancing aesthetic appeal and user-centric functionality.
-                      With a keen eye for branding, I've helped businesses establish distinctive identities that truly resonate with their target audiences.
-                      My proficiency in graphic design and marketing allows me to create compelling visuals and strategies that captivate audiences and drive results.
-I've designed and executed various successful digital marketing campaigns, leveraging my knowledge of search engine optimisation and marketing, as well as using social media to drive engagement and conversions.
+                    <h3>About Me</h3>
+                    <br />
+                    <p>
+                      My journey into the tech industry is anything but conventional.
+                      Without a traditional educational pathway, my career has been driven by a relentless curiosity, a passion for technology and design, and a drive for constant self-learning.
+                    </p>
+                    <br />
+                    <p>
+                  With a firm grasp on popular designing tools and coding languages, and a continuous drive for learning new technologies,
+                  I have built a comprehensive portfolio that reflects a blend of eye-catching aesthetics and functionality.
+                  Over the years, I have honed my skills by immersing myself in an array of freelance projects and collaborations, resulting in a versatile skill set that spans across web design and development, branding, graphic design, marketing, and software engineering - all without a traditional degree.
 
-                        </p></strong>
+                    </p>
+                    <p>
+                      I am a blend of a creative and logical thinker. My creativity sparks innovative ideas, while my logical side ensures their feasibility and alignment with any business objective.
+                     I believe this balance is essential in creating digital solutions that are not only visually stunning but also technically robust.
+                    </p>
                         <br />
 
                     </div>
@@ -94,7 +58,26 @@ I've designed and executed various successful digital marketing campaigns, lever
                         </p>
                     </div>
                 </div>
+
+
+
+
+
+            <div className="text-block">
+                <p>
+                  As a Melbournian, I draw inspiration from my vibrant surroundings and multicultural city, translating my experiences into digital mediums.
+                  The desire to create meaningful and engaging digital interactions has been the north star guiding my career progression.
+                  My work is characterised by meticulous attention to detail, commitment to design ethics, and an ability to translate client requirements into visually compelling and functional designs.
+                </p>
+                 <p>
+                      I've created diverse websites, meticulously balancing aesthetic appeal and user-centric functionality.
+                      With a keen eye for branding, I've helped businesses establish distinctive identities that truly resonate with their target audiences.
+                      My proficiency in graphic design and marketing allows me to create compelling visuals and strategies that captivate audiences and drive results.
+I've designed and executed various successful digital marketing campaigns, leveraging my knowledge of search engine optimisation and marketing, as well as using social media to drive engagement and conversions.
+
+                        </p>
                 <br />
+                </div>
                 <br />
                 <p>
                   Every challenge I undertake is an opportunity to learn, to improve, and to create something remarkable.
@@ -121,7 +104,6 @@ I've designed and executed various successful digital marketing campaigns, lever
                     </a>
                 </p>
             </div>
-        </div>
     );
 };
 
