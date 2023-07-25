cdf378674293286efd6fb76a61d1b1a39a9d30b2
mattyperrott <104394280+mattyperrott@users.noreply.github.com>
2023-07-26 02:28:37 +1000

    Update Desktop.tsx

--- src/components/os/Desktop.tsx
+++ src/components/os/Desktop.tsx
@@ -2,7 +2,6 @@ import React, { useCallback, useEffect, useState } from 'react';
 import Colors from '../../constants/colors';
 import ShowcaseExplorer from '../applications/ShowcaseExplorer';
 import Doom from '../applications/Doom';
-import OregonTrail from '../applications/OregonTrail';
 import ShutdownSequence from './ShutdownSequence';
 import ThisComputer from '../applications/ThisComputer';
 import Toolbar from './Toolbar';
@@ -35,12 +34,7 @@ const APPLICATIONS: {
         shortcutIcon: 'showcaseIcon',
         component: ShowcaseExplorer,
     },
-   // trail: {
-     //   key: 'trail',
-       // name: 'The Oregon Trail',
-      //  shortcutIcon: 'trailIcon',
-       // component: OregonTrail,
-  //  },
+  
     doom: {
         key: 'doom',
         name: 'Doom',
